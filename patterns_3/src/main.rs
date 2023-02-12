a10f788af4f954e22d2bfb2c4ef38c354008d653
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2023-02-12 10:21:12 -0300

    Destructuring to Break Apart Values

--- patterns_3/src/main.rs
+++ patterns_3/src/main.rs
@@ -1,5 +1,17 @@
 #![allow(unused)]
 
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
 fn main() {
     {
         // Matching Literals
@@ -58,5 +70,83 @@ fn main() {
             'k'..='z' => println!("late ASCII letter"),
             _ => println!("something else"),
         }
+    }
+
+    {
+        // Destructuring to Break Apart Values
+
+        // Destructuring Structs
+
+        let p = Point { x: 0, y: 7 };
+
+        let Point { x: a, y: b } = p;
+        assert_eq!(0, a);
+        assert_eq!(7, b);
+
+        let p = Point { x: 0, y: 7 };
+
+        let Point { x, y } = p;
+        assert_eq!(0, x);
+        assert_eq!(7, y);
+        let p = Point { x: 0, y: 7 };
+
+        match p {
+            Point { x, y: 0 } => println!("On the x axis at {x}"),
+            Point { x: 0, y } => println!("On the y axis at {y}"),
+            Point { x, y } => {
+                println!("On neither axis: ({x}, {y})");
+            }
+        }
+    }
+    {
+        // Destructuring Enums
+
+        let msg = Message::ChangeColor(0, 160, 255);
+
+        match msg {
+            Message::Quit => {
+                println!("The Quit variant has no data to destructure.");
+            }
+            Message::Move { x, y } => {
+                println!("Move in the x direction {x} and in the y direction {y}");
+            }
+            Message::Write(text) => {
+                println!("Text message: {text}");
+            }
+            Message::ChangeColor(r, g, b) => {
+                println!("Change the color to red {r}, green {g}, and blue {b}",)
+            }
+        }
+    }
+    {
+        // Destructuring Nested Structs and Enums
+        enum Color {
+            Rgb(i32, i32, i32),
+            Hsv(i32, i32, i32),
+        }
+
+        enum Message {
+            Quit,
+            Move { x: i32, y: i32 },
+            Write(String),
+            ChangeColor(Color),
+        }
+        let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
+
+        match msg {
+            Message::ChangeColor(Color::Rgb(r, g, b)) => {
+                println!("Change color to red {r}, green {g}, and blue {b}");
+            }
+            Message::ChangeColor(Color::Hsv(h, s, v)) => {
+                println!("Change color to hue {h}, saturation {s}, value {v}")
+            }
+            _ => (),
+        }
+    }
+
+    {
+        // Destructuring Structs and Tuples
+
+        let ((feet, inches), Point { x, y }) = ((3, 10), Point { x: 3, y: -10 });
     }
 }
