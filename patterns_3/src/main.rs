0ba124135f84fd9f740b98050e48c59ec7c3bdd5
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2023-02-13 07:52:36 -0300

    extra conditional matchs and @ bindings

--- patterns_3/src/main.rs
+++ patterns_3/src/main.rs
@@ -243,4 +243,55 @@ fn main() {
         }
             */
     }
+
+    {
+        // Extra Conditionals with Match Guards
+
+        let num = Some(4);
+
+        match num {
+            Some(x) if x % 2 == 0 => println!("The number {} is even", x),
+            Some(x) => println!("The number {} is odd", x),
+            None => (),
+        }
+
+        let x = Some(5);
+        let y = 10;
+
+        match x {
+            Some(50) => println!("Got 50"),
+            Some(n) if n == y => println!("Matched, n = {n}"),
+            _ => println!("Default case, x = {:?}", x),
+        }
+
+        println!("at the end: x = {:?}, y = {y}", x);
+
+        let x = 4;
+        let y = false;
+
+        match x {
+            4 | 5 | 6 if y => println!("yes"),
+            _ => println!("no"),
+        }
+    }
+
+    {
+        // @ Bindings
+
+        enum Message {
+            Hello { id: i32 },
+        }
+
+        let msg = Message::Hello { id: 5 };
+
+        match msg {
+            Message::Hello {
+                id: id_variable @ 3..=7,
+            } => println!("Found an id in range: {}", id_variable),
+            Message::Hello { id: 10..=12 } => {
+                println!("Found an id in another range")
+            }
+            Message::Hello { id } => println!("Found some other id: {}", id),
+        }
+    }
 }
